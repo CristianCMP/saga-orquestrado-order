8dac08f7e4fe633e6cbf9544c530b52412b2988d
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-10-22 20:37:10 -0300

    Configuration an automatic topics in the services

--- src/main/java/br/com/saga_orquestrado/order/config/kafka/KafkaConfig.java
+++ src/main/java/br/com/saga_orquestrado/order/config/kafka/KafkaConfig.java
@@ -1,13 +1,16 @@
 package br.com.saga_orquestrado.order.config.kafka;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
@@ -18,6 +21,9 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class KafkaConfig {
 
+    private static final Integer PARTITION_COUNT = 1;
+    private static final Integer REPLICA_COUNT = 1;
+
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
@@ -27,13 +33,19 @@ public class KafkaConfig {
     @Value("${spring.kafka.consumer.auto-offset-reset}")
     private String autoOffsetReset;
 
+    @Value("${spring.kafka.topic.start-saga}")
+    private String startSagaTopic;
+
+    @Value("${spring.kafka.topic.notify-ending}")
+    private String notifyEndingTopic;
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerProperties());
+        return new DefaultKafkaConsumerFactory<>(consumerProps());
     }
 
-    private Map<String, Object> consumerProperties() {
-        Map<String, Object> props = new HashMap<>();
+    private Map<String, Object> consumerProps() {
+        var props = new HashMap<String, Object>();
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
@@ -46,15 +58,15 @@ public class KafkaConfig {
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(producerProperties());
+        return new DefaultKafkaProducerFactory<>(producerProps());
     }
 
-    private Map<String, Object> producerProperties() {
-        Map<String, Object> props = new HashMap<>();
+    private Map<String, Object> producerProps() {
+        var props = new HashMap<String, Object>();
 
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return props;
     }
@@ -64,4 +76,21 @@ public class KafkaConfig {
         return new KafkaTemplate<>(producerFactory);
     }
 
+    private NewTopic buildTopic(String name) {
+        return TopicBuilder
+                .name(name)
+                .partitions(PARTITION_COUNT)
+                .replicas(REPLICA_COUNT)
+                .build();
+    }
+
+    @Bean
+    public NewTopic startSagaTopic() {
+        return buildTopic(startSagaTopic);
+    }
+
+    @Bean
+    public NewTopic notifyEndingTopic() {
+        return buildTopic(notifyEndingTopic);
+    }
 }
