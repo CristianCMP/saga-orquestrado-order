597eef4ef44bbb9f740889e805a1d145c369cdde
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-10-22 21:49:28 -0300

    Structuring classes and DTOs

---
+++ src/main/java/br/com/saga_orquestrado/order/core/document/Product.java
@@ -0,0 +1,13 @@
+package br.com.saga_orquestrado.order.core.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String code;
+    private double unitValue;
+}
